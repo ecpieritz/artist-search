18d9ab214d8779177285ae84c59216f5404e3288
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-11-25 15:02:47 -0300

    Added List component

---
+++ src/components/list/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./index.scss"
+
+class List extends Component {
+  render() {
+    return (
+      <aside className="list col-md-4">
+        <div className="list__item">
+          <a className="list__item__thumbnail" href="#">
+            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
+          </a>
+          <div className="list__item__title">
+            <a href="#"><strong>Video Title</strong></a>
+          </div>
+        </div>
+        <div className="list__item">
+          <a className="list__item__thumbnail" href="#">
+            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
+          </a>
+          <div className="list__item__title">
+            <a href="#"><strong>Video Title</strong></a>
+          </div>
+        </div>
+        <div className="list__item">
+          <a className="list__item__thumbnail" href="#">
+            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
+          </a>
+          <div className="list__item__title">
+            <a href="#"><strong>Video Title</strong></a>
+          </div>
+        </div>
+        <div className="list__item">
+          <a className="list__item__thumbnail" href="#">
+            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
+          </a>
+          <div className="list__item__title">
+            <a href="#"><strong>Video Title</strong></a>
+          </div>
+        </div>
+      </aside>
+    )
+  }
+};
+
+export default List
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import Header from "../../components/header";
+import List from "../../components/list";
 
-class Home extends Component{
-  render(){
-    return(
-      <Header />
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <div className="row">
+            <div>video</div>
+            <List />
+          </div>
+        </div>
+      </>
     )
   }
 };
