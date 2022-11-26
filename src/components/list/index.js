03cebf83249060d30de4ea279f4246baf02dd090
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-11-26 11:42:27 -0300

    Added loadListVideos in list

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,45 +1,38 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import "./index.scss"
 
 class List extends Component {
+  loadListVideos(video) {
+    return (
+      <div className="list__item">
+        <a className="list__item__thumbnail" href="#">
+          <img src={video.snippet.thumbnails.default.url} alt="" />
+        </a>
+        <div className="list__item__title">
+          <a href="#"><strong>{video.snippet.title}</strong></a>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <aside className="list col-md-3">
-        <div className="list__item">
-          <a className="list__item__thumbnail" href="#">
-            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
-          </a>
-          <div className="list__item__title">
-            <a href="#"><strong>Video Title</strong></a>
-          </div>
-        </div>
-        <div className="list__item">
-          <a className="list__item__thumbnail" href="#">
-            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
-          </a>
-          <div className="list__item__title">
-            <a href="#"><strong>Video Title</strong></a>
-          </div>
-        </div>
-        <div className="list__item">
-          <a className="list__item__thumbnail" href="#">
-            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
-          </a>
-          <div className="list__item__title">
-            <a href="#"><strong>Video Title</strong></a>
-          </div>
-        </div>
-        <div className="list__item">
-          <a className="list__item__thumbnail" href="#">
-            <img src="https://kp-blog.s3.amazonaws.com/wp-content/uploads/2018/07/03165544/Thumbnail-Youtube-o-que-%C3%A9-8-dicas-de-como-fazer-e-principais-ferramentas.jpg" />
-          </a>
-          <div className="list__item__title">
-            <a href="#"><strong>Video Title</strong></a>
-          </div>
-        </div>
+        {this.props.videos.map(video =>{
+          return this.loadListVideos(video)
+        })}
       </aside>
     )
   }
 };
 
-export default List
+const mapStateToProps = (state => {
+  return {
+    videos: state.search.videos,
+    loading: state.search.loading,
+    error: state.search.error
+  }
+})
+
+export default connect(mapStateToProps, null)(List)
