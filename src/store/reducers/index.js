7399b9ec92c09cac542c24b56339a5108a371e22
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-11-26 11:51:38 -0300

    Added Video Actions and Reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers} from 'redux';
 import search from './searchRD';
+import startVideo from './startvideoRD';
 
 const getReducers = combineReducers({
-  search
+  search, startVideo
 });
 
 export default getReducers;
